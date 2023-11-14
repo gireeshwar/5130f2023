9d1909f7d10d149b86e4d3f5907105ec2a473d19
gireeshwar <145204648+gireeshwar@users.noreply.github.com>
2023-11-13 22:54:52 -0500

    Add files via upload

---
+++ Gangarapu-GireeshwarChowdary_ProjectProgressReport2/main.js
@@ -0,0 +1,13 @@
+
+import firebase from "firebase/compat";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAMX4bZwT4JpwnVK28hNfWOukcjalXZzUA",
+    authDomain: "iws1-4be46.firebaseapp.com",
+    projectId: "iws1-4be46",
+    storageBucket: "iws1-4be46.appspot.com",
+    messagingSenderId: "245789471783",
+    appId: "1:245789471783:web:a180927220201b036aca02"
+};
+
+ firebase.initializeApp(firebaseConfig);
